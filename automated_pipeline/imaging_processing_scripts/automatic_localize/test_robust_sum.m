dd3910833a7bf4174ddaf57dccf8e0f57c02dfab
Gansheng TAN <49130176+GanshengT@users.noreply.github.com>
2025-08-23 09:12:19 -0500

    Delete automated_pipeline/imaging_processing_scripts/automatic_localize/test_robust_sum.m

--- automated_pipeline/imaging_processing_scripts/automatic_localize/test_robust_sum.m
+++
@@ -1,7 +0,0 @@
-%TEST_ROBUST_SUM Unit tests for robust_sum function.
-%   Verifies handling of quantile bounds and invalid values.
-
-assert(isequal(robust_sum([1 2 3 4], 0.5), 7));
-assert(isequal(robust_sum([1 2 3 4], 2), 10));
-assert(isequal(robust_sum([1 2 3 4], -1), 4));
-assert(isequal(robust_sum([1 2 NaN Inf 5], 0.5), 7));
